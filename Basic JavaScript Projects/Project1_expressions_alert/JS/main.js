4558ac6ba2d8e05cd8be9a8e088feb42fd65bd86
Nathan C Ilescas <nathan.ilescas@outlook.com>
2023-03-14 19:51:09 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,29 @@
-window.alert("Hello World!")
+window.alert("Hello World!");
+document.write("Hello World!");
+
+let A = "This is a string";
+document.write(A);
+
+let B = "This is another string";
+window.alert(B);
+
+let C = "MLK - \"I have a dream\""
+document.write(C);
+
+let D = "Hello" + " " + "World";
+document.write(D);
+
+let E = "Hello", F = "World", G = "GoodBye";
+document.write (F);
+
+let Sent1 =  "This is the beginning of the string";
+let Sent2 = "and this is the end of the string";
+
+Sent1 + Sent2
+
+ let x = "A variable" // This is my Expression
+
+function Add (num1, num2) { //Creating a function
+   let total =  num1 + num2 ; 
+   return total;
+}
